91a171f1f31b3734f3d0fb2c1981be9a40808571
Zeyu Chen <iszeyu.chen@gmail.com>
2024-12-15 23:44:54 +1100

    refactor(project): migrate to Apollo Server and TypeScript

---
+++ src/resolvers.ts
@@ -0,0 +1,15 @@
+import db from './db';
+
+export const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+  },
+};
