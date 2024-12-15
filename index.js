b2f491eed1b40b585b3e68444ff12e0c86889097
Zeyu Chen <iszeyu.chen@gmail.com>
2024-12-15 21:45:56 +1100

    feat(config): setup graphql, graphql-http

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+import { buildSchema } from 'graphql';
+import { createHandler } from 'graphql-http/lib/use/express';
 import routes from './src/routes/crmRoutes';
+
+// Build my schema
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provide resolve function
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
 const app = express();
 const PORT = 3000;
 
-// Set strictQuery option
-mongoose.set('strictQuery', true);
-
-// mongoose connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb');
-
-//bodyparser setup
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 // serving static files
 app.use(express.static('public'));
 
-
 routes(app);
 
-app.get('/', (req, res) => 
-    res.send(`Node and express server is running on port ${PORT}`)
+// Setup GraphQL endpoint
+app.use(
+  '/graphql',
+  createHandler({
+    schema: schema,
+    rootValue: root,
+  })
 );
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
+app.listen(PORT, () =>
+  console.log(`Your server is running on port ${PORT}/graphql`)
 );
